116144984aa2b2dfb3f559e4f00d2bcc2cdb6893
Rodrigo Tolentino Martinez <rodrigotmarzdev@outlook.com>
2025-07-21 21:10:36 -0600

    -Creación del proyecto. -Integración de tailwind para los diseños. -Iconos referentes  a las redes sociales. -Customization del estilo dark. -Configuración de rutas en el archivo tsconfig.ts -Configuracion del astro.config.mjs para correr del lado del servidor. -Integración del lenguaje para el sitio web, cambiar entre ingles y español. -No es necesario el archivo Welcome.astro

---
+++ src/i18n/es.ts
@@ -0,0 +1,222 @@
+const es = {
+    nav: {
+        home: "Inicio",
+        projects: "Proyectos",
+        blog: "Blog",
+        about: "Acerca de",
+        contact: "Contacto",
+    },
+    footer: {
+        title: "¿Trabajamos juntos?",
+        btn: "Contáctame",
+    },
+    home: {
+        banner: {
+            title: `¡Hola, soy
+                <span class="text-primary-700 dark:text-neutral-100 font-semibold">Jonathan Bello</span>!`,
+            title2: "Desarrollador de Software",
+            subtitle: "Creando soluciones tecnológicas llenas de belleza",
+            button: "Mira mis proyectos",
+        },
+        hability: {
+            title: "¿Qué puedo hacer?",
+            card1: {
+                title: "Desarrollo Web",
+                description:
+                    "Especializado en desarrollo web, me dedico a crear experiencias digitales innovadoras y responsivas. Empleo las últimas tecnologías para diseñar sitios web atractivos, rápidos, seguros y optimizados para SEO. Mi enfoque personalizado garantiza una presencia en línea efectiva y profesional, ajustada a las necesidades específicas de cada cliente.",
+            },
+            card2: {
+                title: "Desarrollo Backend",
+                description:
+                    "Con una sólida experiencia en desarrollo backend, me dedico a construir la infraestructura que permite a las aplicaciones web funcionar de manera óptima. Desarrollando soluciones seguras, escalables y de alto rendimiento, garantizo que los sistemas sean eficientes y estén preparados para el crecimiento futuro. Mi prioridad es asegurar que la arquitectura backend proporcione una base robusta y confiable para todas las operaciones.",
+            },
+            card3: {
+                title: "Desarrollo UI",
+                description:
+                    "En diseño UI, creo interfaces visualmente atractivas y funcionales, aprovechando mi experiencia en programación. Esta combinación me permite integrar diseño y desarrollo de manera coherente, ofreciendo soluciones que equilibran estética y funcionalidad para una experiencia de usuario superior.",
+            },
+        },
+        experience: {
+            title: "Experiencia",
+            card0: {
+                title: "Instituto Tecnológico Superior de Teziutlán",
+                duration: "2018 - Presente",
+                role: "Estudiante & Asesor",
+                funciones: [
+                    "Diseño y creación de una plataforma educativa de cursos",
+                    "Maestro de ceremonias",
+                    "Líder y expositor de proyectos",
+                    "Asesoría en proyectos",
+                    "Dictado de cursos",
+                ],
+            },
+            card1: {
+                title: "YGU COMUNICAXION",
+                duration: "2022 - Presente",
+                role: "Web Developer",
+                funciones: [
+                    "Desarrolle varias landing page en Gatsby.js y Astro.js",
+                    "Creación de Aplicaciones web robustas con Next.js",
+                    "Mantenimiento de sitios hechos con Wordpress",
+                    "Uso de GitHub para trabajo colaborativo.",
+                    "Estandarización de proyectos antiguos.",
+                    "Trabajo de diseño UI colaborativo.",
+                    "Implementación de estrategias de SEO.",
+                ],
+            },
+            card2: {
+                title: "AppDeux",
+                duration: "2025 - Presente",
+                role: "Director Técnico",
+                funciones: [
+                    "Administración de proyectos",
+                    "Gestión de equipos",
+                    "Levantamiento de requisitos",
+                    "Desarrollo de herramientas web",
+                    "Análisis de datos",
+                ],
+            },
+        },
+        projects: {
+            title: "Proyectos relevantes",
+            btn: "Ver todos",
+        },
+        about: {
+            title: "Acerca de mi",
+            bio: [
+                {
+                    title: "Nombre",
+                    description: "Jonathan Alexis Bello López",
+                },
+                {
+                    title: "Titulo",
+                    description: "Ingeniero en Sistemas de Computacionales",
+                },
+                {
+                    title: "Especialidad",
+                    description: "Desarrollo Web & IA",
+                },
+                {
+                    title: "Fortalezas",
+                    description:
+                        "Capacidad para combinar el diseño y la programación, ofreciendo soluciones integrales y eficientes. ",
+                },
+                {
+                    title: "Debilidades",
+                    description: "Perfeccionismo y síndrome del impostor.",
+                },
+                {
+                    title: "Pasatiempos",
+                    description: "Videojuegos, lectura, animación y viajes.",
+                },
+                {
+                    title: "Frase",
+                    description: "Creando soluciones tecnológicas llenas de belleza.",
+                },
+            ],
+            titleStack: "Mi Stack principal",
+        },
+        techs: {
+            title: "Tecnologías y Habilidades",
+            titleFrontend: "Frontend",
+            titleBackend: "Backend",
+            titleUi: "Diseño UI",
+            titleDevelopmentTools: "Herramientas de Desarrollo",
+        },
+    },
+    projects: {
+        banner: {
+            title: "Mis Proyectos",
+        },
+        categories: {
+            title: "Categorías",
+        },
+    },
+    project: {
+        title: "Proyectos",
+        comments: {
+            title: "Deja un comentario",
+            name: "Nombre",
+            comment: "Comentario",
+            send: "Enviar",
+        },
+    },
+    about: {
+        title: " ⚔️ Sobre Mi",
+        description:
+            "Un explorador digital que mezcla código, videojuegos y educación para crear experiencias que enseñan y emocionan",
+        cardCharacter: {
+            header: {
+                name: "Jonathan Bello",
+                class: "Clase: Dev del Astronómico",
+                level: "Nivel 25 | Región: Internet",
+                description: `"Jonathan Bello es un dev-artesano con pasión por el conocimiento, capaz
+      de transformar aulas en arenas interactivas. Cree que aprender debe
+      sentirse como desbloquear logros secretos."`,
+            },
+            mainStats: {
+                language: "Lenguaje Principal",
+                framework: "Framework Favorito",
+                skill: "Habilidad Única",
+                arm: "Arma especial",
+
+                languageValue: "JavaScript",
+                frameworkValue: "Astro.js",
+                skillValue: "Gamificación educativa",
+                armValue: "Micrófono Blue Yeti 🎤",
+            },
+        },
+        stats: {
+            dev: "Desarrollo Web",
+            charisma: "Carisma",
+            logic: "Lógica",
+            english: "Inglés",
+            luck: "Suerte",
+        },
+        myHistory: {
+            title: "🗺️ Mi historia",
+            description:
+                "Desde mis inicios en la programación, he estado inmerso en el mundo del desarrollo web. He trabajado en proyectos que van desde aplicaciones simples hasta plataformas complejas, siempre buscando la forma de mejorar mis habilidades y aprender nuevas tecnologías. Mi pasión por el aprendizaje continuo me ha llevado a explorar diversas áreas dentro del desarrollo, incluyendo el diseño UI y la inteligencia artificial.",
+        },
+        myExperience: {
+            title: "💼 Experiencia",
+            description: `<ul>
+          <li>Desarrollador Web fullstack</li>
+          <li>Docencia</li>
+          <li>Investigación: videojuegos educativos con IA</li>
+          <li>Contenido en YouTube y covers musicales 🎙️</li>
+        </ul>
+        `,
+        },
+        myVision: {
+            title: "🌌 Visión",
+            description:
+                "Mi meta es fusionar tecnología, narrativa y pedagogía para que programar sea tan épico como vencer al jefe final de tu videojuego favorito. Estoy comprometido con la creación de entornos donde aprender se sienta como jugar, y busco la forma de mejorar mis habilidades y aprender nuevas tecnologías para lograr este objetivo.",
+        },
+    },
+    contact: {
+        title: "Contáctame",
+        description:
+            "¿Tienes alguna pregunta o proyecto en mente? No dudes en contactarme. Estoy aquí para ayudarte.",
+        form: {
+            name: "Nombre",
+            email: "Correo electrónico",
+            message: "Mensaje",
+            send: "Enviar",
+        },
+        infoContact: {
+            title: "Información de Contacto",
+            emailTitle: "Email",
+            emailValue: "jonathanbello@gmail.com",
+            locationTitle: "Ubicación",
+            locationValue: "Puebla, México",
+            phoneTitle: "Teléfono",
+            phoneValue: "+52 231 325 3031",
+        },
+        social: {
+            title: "Redes Sociales",
+        },
+    },
+};
+
+export default es;
